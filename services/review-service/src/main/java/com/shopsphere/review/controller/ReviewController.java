c4c85818022796bbfc51507ba487cc4163648c7d
kavixz <kavixzneth2002@gmail.com>
2026-02-16 23:48:20 +0530

    chore: update mongodb port and enable cors for local testing

--- services/review-service/src/main/java/com/shopsphere/review/controller/ReviewController.java
+++ services/review-service/src/main/java/com/shopsphere/review/controller/ReviewController.java
@@ -12,10 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/reviews")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @Validated
 public class ReviewController {
