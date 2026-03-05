5f8d311b1423d373c9950f86b0512fc6c61c4a5d
dulhara <wkldulhara@gmail.com>
2026-03-05 13:25:23 +0530

    Revert "merge: order-service MVP Phase 1 into dev"
    This reverts commit 44af39454d6809a446c6b8b991168dc43e31d6ca, reversing
    changes made to 53ef4296b18b3d10a16d2ad2cf278f755a603105.

--- services/order-service/src/main/java/com/shopsphere/order/model/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.shopsphere.order.model;
-
-public enum PaymentMethod {
-    CARD,
-    PAYPAL,
-    CRYPTO
-}
--- services/order-service/src/main/java/com/shopsphere/order/model/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.shopsphere.order.model;
-
-public enum PaymentStatus {
-    PENDING,
-    COMPLETED,
-    FAILED,
-    REFUNDED
-}
